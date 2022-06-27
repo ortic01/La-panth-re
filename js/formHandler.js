765acedc41f5022aec35011d375f15bbe56331e9
ortic01 <chris76200@gmail.com>
2022-06-27 13:59:49 +0200

    first commit

--- js/formHandler.js
+++ js/formHandler.js
@@ -40,9 +40,9 @@ $(function()
 		    		cache: false,
 		    		success: function() // Success
 		 			{  
-						if($form.is('[success-msg]')) // Show Success Message
+						if($form.is('[data-success-msg]')) // Show Success Message
 						{
-							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('success-msg')+"</strong></div></div>");
+							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('data-success-msg')+"</strong></div></div>");
 						}
 						else // Re-Direct
 						{
